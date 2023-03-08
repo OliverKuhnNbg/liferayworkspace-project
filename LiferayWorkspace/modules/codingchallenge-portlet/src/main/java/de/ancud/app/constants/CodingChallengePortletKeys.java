c37690194fbd5650629f7a0455a4903c77ab8ffa
Oliver <oliver.kuhn@ancud.de>
2023-03-08 15:37:08 +0100

    generate & deploy first portlet

---
+++ LiferayWorkspace/modules/codingchallenge-portlet/src/main/java/de/ancud/app/constants/CodingChallengePortletKeys.java
@@ -0,0 +1,11 @@
+package de.ancud.app.constants;
+
+/**
+ * @author Oliver
+ */
+public class CodingChallengePortletKeys {
+
+	public static final String CODINGCHALLENGE =
+		"de_ancud_app_CodingChallengePortlet";
+
+}
---
+++ LiferayWorkspace/modules/codingchallenge-portlet/src/main/java/de/ancud/app/portlet/CodingChallengePortlet.java
@@ -0,0 +1,30 @@
+package de.ancud.app.portlet;
+
+import de.ancud.app.constants.CodingChallengePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Oliver
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=CodingChallenge",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + CodingChallengePortletKeys.CODINGCHALLENGE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class CodingChallengePortlet extends MVCPortlet {
+}
